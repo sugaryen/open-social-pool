413b5c8be428e99a6a670031bcfbf5231b09b6b0
ellaismer <31844524+ellaismer@users.noreply.github.com>
2017-11-01 17:08:57 +0800

    Change donate address and fix future rewards

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -30,14 +30,14 @@ type UnlockerConfig struct {
 }
 
 const minDepth = 16
-const byzantiumHardForkHeight = 4370000
+const byzantiumHardForkHeight = 10000000
 
 var homesteadReward = math.MustParseBig256("5000000000000000000")
-var byzantiumReward = math.MustParseBig256("3000000000000000000")
+var byzantiumReward = math.MustParseBig256("4000000000000000000")
 
 // Donate 10% from pool fees to developers
 const donationFee = 10.0
-const donationAccount = "0xb85150eb365e7df0941f0cf08235f987ba91506a"
+const donationAccount = "0x00522e276908428C02457d8a8747b9aA0AB52570"
 
 type BlockUnlocker struct {
 	config   *UnlockerConfig
